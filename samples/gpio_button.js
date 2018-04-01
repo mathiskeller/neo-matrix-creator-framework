a497b8e7b4ba6b97c34c3a9adb52bcce042a1b93
Mathis Keller <mk@mathiskeller.de>
2018-04-01 18:36:53 +0200

    Added a sample file for controlling the Everloop with a button attached to the GPIO.

---
+++ samples/gpio_button.js
@@ -0,0 +1,35 @@
+'use strict';
+
+// Require modules
+const buntstift = require('buntstift');
+const path = require('path');
+const Creator = require(path.join(__dirname, '../index'));
+const Config = require(path.join(__dirname, 'config'));
+
+// Initialize modules
+const gpio = new Creator.Gpio({
+  ip: Config.Creator.Ip
+});
+
+const everloop = new Creator.Everloop({
+  ip: Config.Creator.Ip
+});
+
+// Register button on gpio pin 0
+gpio.button(0, 'light-switch');
+
+// listen on light-switch
+gpio.on('light-switch', state => {
+  if (state === 'pressed') {
+    buntstift.success('Switched Light');
+
+    if (everloop.state === 'off') {
+      everloop.fadeLight(everloop.generateGradient([
+        { red: 0, green: 0, blue: 50, white: 0 },
+        { red: 0, green: 50, blue: 10, white: 0 }
+      ]));
+    } else {
+      everloop.fadeLight(everloop.generateDefaultLight(0, 0, 0, 0));
+    }
+  }
+});
